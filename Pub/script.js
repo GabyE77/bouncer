92b76a65c3d44dbf2d39e31054abd27755c4a41b
GabyE77 <94193219+GabyE77@users.noreply.github.com>
2022-01-07 21:57:11 +0100

    Add files via upload

---
+++ Pub/script.js
@@ -0,0 +1,27 @@
+const myAge = 18;
+
+
+if (myAge >= 18) {
+
+    console.log("You are 18 years or older");
+
+} else {
+
+    console.log("You are to young");
+}
+
+const isFemale = true;
+
+if (isFemale = true) {
+    console.log('you can enter!');
+} else {
+    console.log('you can not enter!');
+}
+
+const driverStatus = 'designated';
+
+if (driverStatus = "designated") {
+    console.log('you are allowed to drive');
+} else {
+    console.log('youare not allowed to drive');
+}
